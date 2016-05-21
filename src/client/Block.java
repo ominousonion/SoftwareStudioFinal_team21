5f7be97e3623a4fd0c25fb590a07b3ddf565162c
ymmij1996 <ymmij1996@gmail.com>
2016-05-21 20:21:41 +0800

    no message

--- src/client/Block.java
+++ src/client/Block.java
@@ -1,5 +1,6 @@
 package client;
 
+<<<<<<< HEAD
 import processing.core.PApplet;
 
 public class Block extends MapComponent{
@@ -14,4 +15,19 @@ public class Block extends MapComponent{
 		this.applet.rect(x, y, width, width);
 	}
 
+=======
+import processing.core.PImage;
+
+public class Block extends MapComponent{
+	Block(MainApplet applet,int x,int y, int width, int height){
+		super(applet,x,y,width,height);
+		this.img= this.applet.loadImage("block.png");
+	}
+	public void display(){
+		this.applet.image(this.img, this.x, this.y, this.width, this.height);
+		/*this.applet.fill(255,255,0);
+		this.applet.rect(x, y, 5, 5);*/
+		
+	}
+>>>>>>> 2302129ff8510d15a93f56e97b698699cd881a60
 }
