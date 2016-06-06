ffa69a9a422979de8952f4227a180ed03d99174a
ax2082197 <ax2082197@gmail.com>
2016-06-06 19:01:57 +0800

    no message

--- src/client/CharacterState.java
+++ src/client/CharacterState.java
@@ -1,12 +1,14 @@
 package client;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class CharacterState {
 	
 	int x,y,width,height, op_num;
 	private MainApplet applet;
-	private int Max_occupies = 16;
+	private int Max_occupies = 5;
+	PImage ch1, ch2;
 	
 	CharacterState(MainApplet applet){
 		this.x=0;
@@ -16,6 +18,10 @@ public class CharacterState {
 		this.applet=applet;
 		if(applet.map.seq==1) op_num = 2;
 		else op_num = 1;
+		ch1 = applet.loadImage("./src/img/character1_up.png");
+		ch1.resize(100, 100);
+		ch2 = applet.loadImage("./src/img/character2_up.png");
+		ch2.resize(100, 100);
 		//Max_occupies = applet.map.Max_Occupipe;
 	}
 	
@@ -40,6 +46,8 @@ public class CharacterState {
 		this.applet.fill( 0, 0, 0);
 		this.applet.textSize(15);
 		this.applet.text(applet.map.character.ocpy+" / "+Max_occupies, 130, this.height/2-35);
+		if(op_num==2) this.applet.image(ch1, 80, 80);
+		else this.applet.image(ch2, 80, 80);
 		
 		this.applet.fill(65, 129, 129);
 		this.applet.rect(x, height/2, width, height/2);
@@ -59,5 +67,7 @@ public class CharacterState {
 		this.applet.fill( 0, 0, 0);
 		this.applet.textSize(15);
 		this.applet.text(applet.map.opponent.ocpy+" / "+Max_occupies, 130, this.height-35);
+		if(op_num==2) this.applet.image(ch2, 80, 415);
+		else this.applet.image(ch1, 80, 415);
 	}
 }
