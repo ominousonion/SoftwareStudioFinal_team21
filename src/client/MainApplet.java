fd8e46f94257fbd4f73ce0b7be5762df18096792
ominousonion <kenny91320@gmail.com>
2016-06-06 16:10:41 +0800

    no message

--- src/client/MainApplet.java
+++ src/client/MainApplet.java
@@ -24,7 +24,7 @@ public class MainApplet extends PApplet{
 	private Button btn;//Paul added
 	private BackButton backbtn;
 	private PictureSelButton picSelButton;
-	private PicData picdata1;
+	public PicData picdata1;
 	private PImage [][]itemImg = new PImage[100][100];
 	private PImage []backImg = new PImage[10];
 	private PImage MainImage = new PImage();
@@ -224,14 +224,22 @@ public class MainApplet extends PApplet{
 			break;
 		case KeyEvent.VK_Z :
 			if( isStart == false && isExplain == false && isSelected == false ){
-				gc.sendMessage("create");
-				this.map.character.skillCreateBlock.toMakeBlock();				
+				if(this.map.character.money >= 25){
+					gc.sendMessage("create");
+					if(this.map.character.skillCreateBlock.toMakeBlock()){
+						this.map.character.money-=25;							
+					}
+				}			
 			}
 			break;
 		case KeyEvent.VK_X :
 			if( isStart == false && isExplain == false && isSelected == false ){
-				gc.sendMessage("break");
-				this.map.character.skillDeleteBlock.toDeleteBlock();				
+				if(this.map.character.money >= 25){
+					gc.sendMessage("break");
+					if(this.map.character.skillDeleteBlock.toDeleteBlock()){
+						this.map.character.money-=25;
+					}	
+				}				
 			}
 			break;
 		case KeyEvent.VK_SPACE:
@@ -250,8 +258,10 @@ public class MainApplet extends PApplet{
 				if(index>=0 && index<225){
 					com=this.map.components.get(index);
 					if(com.type>=1 && com.type<=3){
-						gc.sendMessage("occupipe");
-						com.occupipe(this.map.character.number);
+						if(map.character.money >= 100){
+							gc.sendMessage("occupipe");
+							this.map.character.skillOccupipeBlock.toOccupipeBlock();
+						}
 					}
 				}				
 				
